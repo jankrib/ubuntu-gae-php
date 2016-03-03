0178dfcad15096d967d9f92b6e482501d7bf295a
Jan Kristian Bjerke <jan@crondale.no>
2016-03-03 10:54:40 +0100

    Add file test

--- main.php
+++ main.php
@@ -1,4 +1,11 @@
 <?php
 
+echo '<h2>Test</h2>';
+
+file_put_contents('gs://my_bucket/hello.txt', 'File operations work!');
+echo file_get_contents('gs://my_bucket/hello.txt');
+
+echo '<h2>PHP info</h2>';
 // Show all information, defaults to INFO_ALL
+
 phpinfo();
